d68fd5c3ec4da370dbbe3f5947349dd86f328329
Varan Gulati <53681461+VTPrince@users.noreply.github.com>
2022-07-05 20:05:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def get_my_ip():
     for key,value in places.items():
         return '<h1>final list : '+ jsonify(hotels)
         # return render_template("table_list.html", data = places,city=str(ip.city))
-    return '<h1>final list : '+ str(ip.city)) #request.remote_addr
+    return '<h1>final list : '+ str(ip.city) #request.remote_addr
 if __name__ == '__main__':
     app.run(debug=True)
 
