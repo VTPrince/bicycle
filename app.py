d38125d7bf1c2b23f6bbc046ec2d67ba1245a84a
Varan Gulati <53681461+VTPrince@users.noreply.github.com>
2022-07-05 20:10:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -85,8 +85,8 @@ def get_my_ip():
     print(places)
 
     for key,value in places.items():
-        # return render_template("table_list.html", data = places,city=str(ip.city))
-    return '<h1>final list : '+ places #request.remote_addr
+        #return render_template("table_list.html", data = places,city=str(ip.city))
+    return '<h1>final list : ' #request.remote_addr
 if __name__ == '__main__':
     app.run(debug=True)
 
