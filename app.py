2df6d326d1917161101a080732a0c4ac57340f0c
Varan Gulati <53681461+VTPrince@users.noreply.github.com>
2022-07-11 00:08:59 +0530

    added new functionality

--- app.py
+++ app.py
@@ -2,24 +2,30 @@
 #     app.run(debug=True)
 from flask import request,Flask,render_template,session,redirect
 from flask import jsonify
+from datetime import timedelta
 from flask_session import Session
 import geocoder
 import firebase_admin
+from firebase_admin.auth import UserRecord
 import pyrebase
 import json
 from firebase_admin import credentials, auth
+from firebase import firebase
 from functools import wraps
 emaily=[]
 jwtoken=[]
+namely=[]
 passwordy=[]
 app = Flask(__name__,template_folder='templates')
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.permanent_session_lifetime = timedelta(minutes=10)
 Session(app)
 
 
 @app.route("/")
 def index():
+    
     return render_template("index.html")
 @app.route('/user_signup',methods=['POST','GET'])
 def user_signup():
@@ -32,6 +38,7 @@ def user_login():
 cred = credentials.Certificate('fbAdminConfig.json')
 firebase = firebase_admin.initialize_app(cred)
 pb = pyrebase.initialize_app(json.load(open('fbconfig.json')))
+db = pb.database()
 #Data source
 users = []
 #Api route to get users
@@ -41,19 +48,25 @@ users = []
 def signup():
     email = request.form['email'] 
     password = request.form['password']
+    name=request.form['name']
     emaily.append(email)
     passwordy.append(password)
+    namely.append(name)
     if email is None or password is None:
         return {'message': 'Error missing email or password'},400
     try:
         user = auth.create_user(
                email=email,
-               password=password
+               password=password,display_name=name
         )
-        return {'message': f'Successfully created user {user.uid}'},200
+        data = {"name": name,"email":email}
+        db.child("users").child(name).set(data)
+
+        return {'message': f'Successfully created user {user.display_name}'},200
         #return redirext('/token')
     except Exception as e:
-        return {'message': 'Error creating user'+e},400
+        print(e)
+        return {'message': "error"},400
 
 #Api route to get a new token for a valid user
 @app.route('/token',methods=["POST","GET"])
@@ -95,14 +108,18 @@ def login():
     email = request.form.get('email')
     #emaily=emaily[0]
     emaily.append(email)
-    
+    user_name=db.child("users").order_by_child("email").equal_to('kohli@gmail.com').get()
+    for un in user_name.each():
+        namely.append(un.key()) 
+    print(namely[0])
     #password=passwordy[0]
     password = request.form.get('password')
     passwordy.append(password)
     try:
         user = pb.auth().sign_in_with_email_and_password(email, password)
         session['user']=user
-        return {'message': "user logged in"}, 200
+        return redirect('/')
+        #return {'message': "user logged in"}, 200
     except Exception as e:
         print(e)
         return {'messahe':'failed'},400
@@ -112,6 +129,12 @@ def login():
 @check_token
 def userinfo():
     return {'data': users}, 200
+
+
+
+#store user data to database
+
+
 
 
 
@@ -168,7 +191,7 @@ def get_my_ip():
     # keep looping upto length of y
     for i in range(len(y)):
         
-        url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins="+str(ip.city)+"&destinations="+y[i]['name']+"units=imperial&key=AIzaSyAl-KQ2kYmnje9S-BXptm8f2X5a_GFBJzg"
+        #url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins="+str(ip.city)+"&destinations="+y[i]['name']+"units=imperial&key=AIzaSyAl-KQ2kYmnje9S-BXptm8f2X5a_GFBJzg"
         print(str(ip.city))
         print(y[i]['name'])
         #hotels.append(y[i]['name'])
@@ -196,6 +219,13 @@ def get_my_ip():
     for key,value in places.items():
         return render_template("table_list.html", data = places,city=str(ip.city))
     return render_template("table_list.html", data = places,city=str(ip.city))
+
+@app.route('/adddestination/<string:place>',methods=['GET','POST'])
+def adddestination(place):
+    user_place=place
+    print(namely[0])
+    db.child("users").child(namely[0]).update({"place": user_place}) 
+    return {'message':'destination added'},200
 if __name__ == '__main__':
     app.run(debug=True)
 
